6a83e94f91d246b9dc719a600dd5524877a62493
AbhilakshayAnand <aa8518@srmist.edu.in>
2025-01-11 02:04:17 +0530

    initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,25 @@
+import HeaderBox from '@/components/HeaderBox'
+import React from 'react'
+
+const Home = () => {
+
+  const loggedIn = {firstName: 'Brawler'}
+
+  return (
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type = 'greeting'
+            title = 'Welcome'
+            user = {loggedIn?.firstName || 'Guest'}
+            subtext = 'Manage your account and transactions efficiently'
+
+          />
+        </header>
+      </div>
+    </section>
+  )
+}
+
+export default Home
